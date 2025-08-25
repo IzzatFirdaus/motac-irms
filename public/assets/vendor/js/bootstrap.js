7c375d29d5eeebebe82297505c040edc91bfdbd6
IzzatFirdaus <2021601074@student.uitm.edu.my>
2025-08-25 08:14:22 +0800

    fix: Tailwind config, document SCSS warnings, update MYDS design docs, and vendor asset hashes for v4.0 compatibility

--- public/assets/vendor/js/bootstrap.js
+++ public/assets/vendor/js/bootstrap.js
@@ -9462,7 +9462,7 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkvuexy"] = self["webpackChunkvuexy"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkmotac_irms"] = self["webpackChunkmotac_irms"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	}();
