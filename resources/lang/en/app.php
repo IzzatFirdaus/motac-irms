b882ce6c3201e14a7e39197eb9cf773379b6c580
IzzatFirdaus <2021601074@student.uitm.edu.my>
2025-09-02 08:23:17 +0800

    fix(i18n): correct parse error in resources/lang/en/app.php

---
+++ resources/lang/en/app.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    // Minimal keys to satisfy editor/language-server checks
+    'system_name'     => 'MOTAC Integrated Resource Management System',
+    'motac_full_name' => 'Ministry of Tourism, Arts and Culture Malaysia',
+];
